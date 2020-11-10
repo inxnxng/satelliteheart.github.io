8eeb7bc2fd5d2390d894316c785ebdf397dfb0b1
satelliteheart29 <red131729@khu.ac.kr>
2020-11-10 19:41:13 +0900

    f

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": "http://localhost:4000/assets/images/star.jpg"
       },{
         "title": "finally",
-        "excerpt":"github blog를 완성하였다. 저번에는 어려워서 포기했지만 이번에는 포기하지 않았다는 점에서 스스로에게 박수를 쳐주고 싶다. 이제 고민해야 하는 것은 포스팅을 어떻게 하면 더 쉽게 할 수 있느냐 이다. 이렇게 매번 git을 통해 올리기는 귀찮은데..   GEM이 신기하다.   참고 링크           minimal mistakes를 이용해 page 만들기            CSS - SCSS 관련 설정하기            프로필 관련 설정       앞으로 더 참고할만한 링크가 있으면 올리도록 해야겠다.  ","categories": ["diary"],
+        "excerpt":"github blog를 완성하였다. 저번에는 어려워서 포기했지만 이래저래 필요성을 느꼈기에 오늘 마음먹고 끝내버렸다. 아직 카테고리를 수정하지 못하지만 기말고사 끝나고 파일 올리면서 수정해보도록 해야겠다. 그보다 GEM이 제일 신기하다. 세상이 많이 발전했구나.   참고 링크           minimal mistakes를 이용해 page 만들기            CSS - SCSS 관련 설정하기            프로필 관련 설정       수정해야 할 것들      카테고리 수정   sitemap.xml 설정   이미지 파일 넣기 확인  ","categories": ["diary"],
         "tags": [],
         "url": "http://localhost:4000/diary/first-post/",
         "teaser": "http://localhost:4000/assets/images/star.jpg"
