94366283e73e5d12f24eac68179713b4f0f96642
satelliteheart29 <red131729@khu.ac.kr>
2020-11-10 23:08:15 +0900

    f

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,6 +8,6 @@ var store = [{
         "title": "finally",
         "excerpt":"github blog를 완성하였다. 저번에는 어려워서 포기했지만 이래저래 필요성을 느꼈기에 오늘 마음먹고 끝내버렸다. 아직 카테고리를 수정하지 못하지만 기말고사 끝나고 파일 올리면서 수정해보도록 해야겠다. 그보다 GEM이 제일 신기하다. 세상이 많이 발전했구나.   참고 링크           minimal mistakes를 이용해 page 만들기            CSS - SCSS 관련 설정하기            프로필 관련 설정       수정해야 할 것들      카테고리 수정   sitemap.xml 설정 -&gt; 이거는 등록해놓고 며칠 걸린다고 한다   이미지 파일 넣기 확인  ","categories": ["diary"],
         "tags": [],
-        "url": "http://localhost:4000/diary/first-post/",
+        "url": "http://localhost:4000/diary/finally/",
         "teaser": "http://localhost:4000/assets/images/star.jpg"
       }]
